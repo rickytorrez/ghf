9020e199ac2162d719bb76796aa2c404489101c2
rickytorrez <torrez.ricky@ymail.com>
2020-06-03 11:32:39 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 
 class App extends Component {
@@ -37,19 +39,32 @@ class App extends Component {
     // de-structuring the state
     const { users, loading, alert } = this.state;
     return (
-      <h1>
-        <Navbar />
-        <Alert alert={alert} />
-        <Search
-          searchUsers={this.searchUsers}
-          clearUsers={this.clearUsers}
-          showClear={users.length > 0 ? true : false}
-          setAlert={this.setAlert}
-        />
-        <div className="container">
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </h1>
+      </Router>
     );
   }
 }
