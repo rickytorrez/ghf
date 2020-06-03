b53ba613db2639690fa11d776956b75d8f24c1b1
rickytorrez <torrez.ricky@ymail.com>
2020-06-03 10:45:16 -0400

    search component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,52 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
-  async componentDidMount() {
+  // search github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
     const response = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+    this.setState({ users: response.data.items, loading: false });
+  };
 
-    this.setState({ users: response.data, loading: false });
-  }
+  // clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // setAlert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    // get rid of alert after five seconds
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
 
   render() {
+    // de-structuring the state
+    const { users, loading, alert } = this.state;
     return (
       <h1>
         <Navbar />
+        <Alert alert={alert} />
+        <Search
+          searchUsers={this.searchUsers}
+          clearUsers={this.clearUsers}
+          showClear={users.length > 0 ? true : false}
+          setAlert={this.setAlert}
+        />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </h1>
     );
