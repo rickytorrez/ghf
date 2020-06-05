5494189e9c72f78e6f89793daa03cf666604f2ce
rickytorrez <torrez.ricky@ymail.com>
2020-06-05 14:20:19 -0400

    finished app

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <p class="lead">Not all who wonder are lost.</p>
+    </div>
+  );
+};
+
+export default NotFound;
